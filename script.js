a60ae5943e778dc37d505ed2bfb796974c3a1235
Illya Tochynskyi <naehk532@gmail.com>
2025-09-21 15:52:45 +0300

    add js btn new task

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
+const textArea = document.getElementById("placeNewTask");
+const buttonNewTask = document.getElementById("btnNewTask");
+const listGroup = document.getElementById("list");
+const listElement = document.getElementsByClassName("element-list");
+const buttonDeleteTask = document.getElementsByClassName("btn-close");
 
-const textArea = 0;
+buttonNewTask.addEventListener ('click', () => {
+    event.preventDefault()
+    const addNewTask = function () {
+    
+    if (textArea.value.trim() === '') {
+        alert('nothing')
+        
+        return ;
 
-const listGroup = 0;
-const listElement = 0;
+    } else {
 
-const buttonNewTask = 0;
-const buttonDeleteTask = 0;
+        const newLi = document.createElement('li');
+        newLi.classList.add("element-list");
+
+        const newTaskText = document.createElement('a');
+        newTaskText.setAttribute("href" , '#!')
+        newTaskText.classList.add('task-text');
+        newTaskText.textContent =  textArea.value;
+
+        const bntDelete = document.createElement('button');
+        bntDelete.classList.add('btn-close');
+        bntDelete.setAttribute('aria-label', 'Close')
+
+        newLi.appendChild(newTaskText);
+        newLi.appendChild(bntDelete);
+
+        listGroup.appendChild(newLi);
+
+    }
+}   
+        addNewTask();
+});
+
+    
+
+
